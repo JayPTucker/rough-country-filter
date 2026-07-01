ddc97f8155450ce0107d708f2325045a8a1f4b0e
Jay Paul Tucker <jaypaultucker@gmail.com>
2026-07-01 17:58:32 -0500

    Created Front-end Design for cards
    - Added Front-end
    - Automatic imports for data in products.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
 import { MOCK_PRODUCTS } from "@/data/products";
 
-export default function Home() {
-  console.log(MOCK_PRODUCTS);
+import "./globals.css";
 
+export default function Home() {
   return (
     <main className="max-w-5xl mx-auto p-8">
-      <h1 className="text-3xl font-bold">
-        Rough Country Product Filter
+      <h1 className="text-3xl font-bold mb-6">
+        Rough Country Product Filter Assessment
       </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {MOCK_PRODUCTS.map((product) => (
+          <div
+            key={product.id}
+            className="productCard"
+          >
+            <p className="productTitle">
+              {product.name}
+            </p>
+
+            <p>
+              {product.year} {product.make} {product.model}
+            </p>
+
+            <p className="font-medium">
+              {product.price.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+
+            
+            <p className={`font-medium ${product.inStock ? "text-green-600" : "text-red-600"}`}>
+              {product.inStock ? "✔ In Stock" : "✘ Out of Stock"}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
